b4062dbd50ee185226f4b76968d08352ec94a948
Ben Pomerenke <ben.pomerenke@asolutions.com>
2016-11-19 23:19:09 -0600

    004 - extracted common code - BP

--- AngularDemoApp/AngularDemoApp/Scripts/app/apiService.js
+++ AngularDemoApp/AngularDemoApp/Scripts/app/apiService.js
@@ -4,12 +4,13 @@
     var angularDemoApp = angular.module('AngularDemoApp');
 
     angularDemoApp.service('apiService', function ($http) {
-
+        var successCallback = function (response) { return response.data; };
+        var failureCallback = function (response) { return console.log("something went wrong");}
         var _getLists = function() {
-            return $http({ method: "GET", url: "api/v1/lists" }).then(function (response) { return response.data; }, function (response) { return console.log("something went wrong");});
+            return $http({ method: "GET", url: "api/v1/lists" }).then(successCallback, failureCallback);
         }
         var _addItem = function(listId, item) {
-            return $http({ method: "POST", url: "api/v1/lists/" + listId + "/item", data: {name: item} }).then(function (response) { return response.data; }, function (response) { return console.log("something went wrong");});
+            return $http({ method: "POST", url: "api/v1/lists/" + listId + "/item", data: { name: item } }).then(successCallback, failureCallback);
         }
         return {
             getLists: _getLists,
