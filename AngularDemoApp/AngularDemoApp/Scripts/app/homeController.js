aa9d528eeefc512a981bd3a4d86e1af77006932e
Ben Pomerenke <ben.pomerenke@asolutions.com>
2016-11-19 22:54:46 -0600

    003 - fetching list data from api - BP

--- AngularDemoApp/AngularDemoApp/Scripts/app/homeController.js
+++ AngularDemoApp/AngularDemoApp/Scripts/app/homeController.js
@@ -3,18 +3,19 @@
 
     var angularDemoApp = angular.module('AngularDemoApp');
 
-    angularDemoApp.controller('homeController', function () {
+    angularDemoApp.controller('homeController', function ($http) {
         var ctrl = this;
 
-        ctrl.lists = [
-            { id: 1, name: 'something', items: ['item 1', 'item 2'] },
-            { id: 2, name: 'something else', items: ['item 3', 'item 4'] }
-        ];
+        ctrl.lists = [];
 
         ctrl.selectedList = undefined;
 
         ctrl.selectList = function(list) {
             ctrl.selectedList = list;
+        }
+
+        ctrl.init = function() {
+            $http({ method: "GET", url: "api/v1/lists" }).then(function(response) { ctrl.lists = response.data; }, function(response) { console.log("something went wrong") });
         }
     });
 })();
