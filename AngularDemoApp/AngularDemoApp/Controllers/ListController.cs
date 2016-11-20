2b2f8de0ed5ceaf7e28e84f31fe9f6dfed7b4efc
Ben Pomerenke <ben.pomerenke@asolutions.com>
2016-11-19 23:16:59 -0600

    004 - persisting list item - BP

--- AngularDemoApp/AngularDemoApp/Controllers/ListController.cs
+++ AngularDemoApp/AngularDemoApp/Controllers/ListController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -11,19 +12,30 @@ namespace AngularDemoApp.Controllers
         [Route("api/v1/lists")]
         public List<ListView> Get()
         {
-            return new List<ListView>
-            {
-                new ListView{ Id = 1, Name = "api list 1", Items = new List<string>{"api item 1", "api item 2"}},
-                new ListView{ Id = 2, Name = "api list 2", Items = new List<string>{"api item 3", "api item 4"}},
-            };
+            return InMemoryData.Lists;
         }
 
         [HttpPost]
         [Route("api/v1/lists/{listId}/item")]
         public HttpResponseMessage AddItem(int listId, ItemView item)
         {
-            return new HttpResponseMessage(HttpStatusCode.OK);
+            var list = InMemoryData.Lists.FirstOrDefault(x => x.Id == listId);
+            if (list != null)
+            {
+                list.Items.Add(item.Name);
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            return new HttpResponseMessage(HttpStatusCode.NotFound);
         }
+    }
+
+    public static class InMemoryData
+    {
+        public static List<ListView> Lists = new List<ListView>
+        {
+            new ListView {Id = 1, Name = "api list 1", Items = new List<string> {"api item 1", "api item 2"}},
+            new ListView {Id = 2, Name = "api list 2", Items = new List<string> {"api item 3", "api item 4"}},
+        };
     }
 
     public class ListView
