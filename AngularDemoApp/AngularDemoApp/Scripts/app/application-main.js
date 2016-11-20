6e2fca778882b1892315eb5d0e9483ff8da4d26f
Ben Pomerenke <ben.pomerenke@asolutions.com>
2016-11-19 22:29:20 -0600

    001 - wired up angular applicatoin - BP

---
+++ AngularDemoApp/AngularDemoApp/Scripts/app/application-main.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    var angularDemoApp = angular.module('AngularDemoApp', ['ngRoute']);
+
+    angularDemoApp.config(function ($routeProvider) {
+        $routeProvider
+            // route for the home page
+            .when('/', {
+                templateUrl: 'home.html',
+                controller: 'homeController',
+                controllerAs: 'ctrl'
+            });
+    });
+})();
