282b68db72742bf808de09f8004222d275ea3bb1
Ben Pomerenke <ben.pomerenke@asolutions.com>
2016-11-19 22:36:06 -0600

    002 - adding lists - BP

--- AngularDemoApp/AngularDemoApp/Scripts/app/homeController.js
+++ AngularDemoApp/AngularDemoApp/Scripts/app/homeController.js
@@ -4,6 +4,11 @@
     var angularDemoApp = angular.module('AngularDemoApp');
 
     angularDemoApp.controller('homeController', function () {
+        var ctrl = this;
 
+        ctrl.lists = [
+            { id: 1, name: 'something', items: ['item 1', 'item 2'] },
+            { id: 2, name: 'something else', items: ['item 3', 'item 4'] }
+        ];
     });
 })();
